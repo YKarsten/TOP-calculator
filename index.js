14e3f56be1face85c6251fe3bee66ceceac2047a
YKarsten <yannik.karsten@web.de>
2022-11-06 12:04:46 +0100

    Add display functionality to the calculator screen

--- index.js
+++ index.js
@@ -29,3 +29,91 @@ function operate(operator, num1, num2){
     }
 }
 
+//Get the screen div to display inputs
+let display= document.getElementById("screen")
+
+
+const zero = document.getElementById("zero");
+zero.addEventListener("click", ()=>{
+    display.textContent += "0";
+})
+
+const one = document.getElementById("one");
+one.addEventListener("click", ()=>{
+    display.textContent += "1";
+})
+
+const two = document.getElementById("two");
+two.addEventListener("click", ()=>{
+    display.textContent += "2";
+})
+
+const three = document.getElementById("three");
+three.addEventListener("click", ()=>{
+    display.textContent += "3";
+})
+
+const four = document.getElementById("four");
+four.addEventListener("click", ()=>{
+    display.textContent += "4";
+})
+
+const five = document.getElementById("five");
+five.addEventListener("click", ()=>{
+    display.textContent += "5";
+})
+
+const six = document.getElementById("six");
+six.addEventListener("click", ()=>{
+    display.textContent += "6";
+})
+
+const seven = document.getElementById("seven");
+seven.addEventListener("click", ()=>{
+    display.textContent += "7";
+})
+
+const eight = document.getElementById("eight");
+eight.addEventListener("click", ()=>{
+    display.textContent += "8";
+})
+
+const nine = document.getElementById("nine");
+nine.addEventListener("click", ()=>{
+    display.textContent += "9";
+})
+
+const calcDivide = document.getElementById("divide");
+calcDivide.addEventListener("click", ()=>{
+    display.textContent += "/";
+})
+
+const calcMultiply = document.getElementById("multiply");
+calcMultiply.addEventListener("click", ()=>{
+    display.textContent += "*";
+})
+
+const minus = document.getElementById("minus");
+minus.addEventListener("click", ()=>{
+    display.textContent += "-";
+})
+
+const plus = document.getElementById("plus");
+plus.addEventListener("click", ()=>{
+    display.textContent += "+";
+})
+
+const sum = document.getElementById("sum");
+sum.addEventListener("click", ()=>{
+    display.textContent += "=";
+})
+
+const undo = document.getElementById("undo");
+undo.addEventListener("click", ()=>{
+    display.textContent = display.textContent.slice(0,-1);
+})
+
+const clear = document.getElementById("clear");
+clear.addEventListener("click", ()=>{
+    display.textContent = "";
+})
